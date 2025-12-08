19d40f1f4726b99c647beace934ad61c370c67d4
soorena <soorenarad99@gmail.com>
2025-12-08 18:14:46 +0330

    fix issue with variable typo and update requirements.txt for async

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from setting import settings
 
-engine = create_async_engine(settings.DB_UR, echo=True)
+engine = create_async_engine(settings.DB_URL, echo=True)
 
 AsyncSessionLocal = sessionmaker(
     engine,               # bind the engine here
